14c7591f7ef689d2a19ec7523a971451df279f95
anthonyv449 <anthonyv449@gmail.com>
2025-11-24 12:51:57 -0800

    add changes to disbale tiktok and implement validation on character count

---
+++ lb-marketing-ui/src/components/ui/tooltip.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import { cn } from "../../lib/utils"
+
+interface TooltipProps {
+  content: string
+  children: React.ReactNode
+  className?: string
+}
+
+export function Tooltip({ content, children, className }: TooltipProps) {
+  return (
+    <div className={cn("relative group inline-block", className)}>
+      {children}
+      <div className="absolute z-50 px-2 py-1 text-xs text-white bg-gray-900 rounded shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 pointer-events-none whitespace-nowrap bottom-full left-1/2 transform -translate-x-1/2 mb-2">
+        {content}
+        <div className="absolute top-full left-1/2 transform -translate-x-1/2 -mt-1">
+          <div className="border-4 border-transparent border-t-gray-900"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
